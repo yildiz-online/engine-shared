f6e264a0f1026d123e9dae9b337f155fd97fac8e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-06-15 12:25:58 +0200

    [NO-JIRA] Mapper implementations.

---
+++ src/main/java/be/yildiz/shared/protocol/mapper/Point3DMapper.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ * Copyright (c) 2017 Grégory Van den Borre
+ *
+ * More infos available: https://www.yildiz-games.be
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ */
+
+package be.yildiz.shared.protocol.mapper;
+
+import be.yildiz.common.vector.Point3D;
+import be.yildiz.module.network.exceptions.InvalidNetworkMessage;
+import be.yildiz.module.network.protocol.MessageSeparation;
+import be.yildiz.module.network.protocol.ObjectMapper;
+
+import java.security.InvalidParameterException;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class Point3DMapper implements ObjectMapper<Point3D> {
+
+    @Override
+    public Point3D to(String s) throws InvalidNetworkMessage {
+        try {
+            return new Point3D(s);
+        } catch (final InvalidParameterException e) {
+            throw new InvalidNetworkMessage("Error retrieving Point3D", e);
+        }
+    }
+
+    @Override
+    public String from(Point3D p) {
+        final float eps = 0.001f;
+        if (p.y < eps && p.y > -eps) {
+            return String.valueOf(p.x) +
+                    MessageSeparation.COLLECTION_SEPARATOR +
+                    p.z;
+        } else {
+            return p.toString();
+        }
+    }
+}
